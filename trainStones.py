03a8b7cd92f3dad1c0c634bd5ab5327164799bdf
DavidMM34 <86305379+DavidMM34@users.noreply.github.com>
2021-06-22 16:43:54 +0100

    Update trainStones.py

--- trainStones.py
+++ trainStones.py
@@ -11,12 +11,13 @@ from tensorflow.keras.callbacks import ModelCheckpoint
 classLevel = 2
 batch_size = 32
 epochs = 25
+mod_ver = 1
 
 datasetPath = "rocks_db/"
 #datasetPath = "D:"
 resultsPath = "results/predict/"
 modelsPath = "models/"
-modelFileName= "jbdm_v0_L" + str(classLevel) + "_B" + str(batch_size) + "_E" + str(epochs) + ".hdf5"
+modelFileName = "jbdm_v" + str(mod_ver) + "_L" + str(classLevel) + "_B" + str(batch_size) + "_E" + str(epochs) + ".hdf5"
 rocks_db = "rocks_db/rocks_db_corrected.json"
 stone_classes_list_L0 = "stone_classes_list_L0.json"
 stone_classes_list_L1 = "stone_classes_list_L1.json"
@@ -76,10 +77,62 @@ class BatchLossHistory(tf.keras.callbacks.Callback):
 
     def on_batch_end(self, batch, logs={}):
         self.batch_losses.append(logs.get('loss'))
-        self.batch_accuracies.append(logs.get('acc'))
+        self.batch_accuracies.append(logs.get('accuracy'))
 
 
 # batch_history = BatchLossHistory()
+
+
+class MetricsValues(tf.keras.callbacks.Callback):
+    def __init__(self, figsize=None):
+        super(MetricsValues, self).__init__()
+        self.figsize = figsize
+
+    def on_train_begin(self, logs={}):
+        self.i = 0
+        self.x = []
+        self.losses = []
+        self.val_losses = []
+        self.acc = []
+        self.val_acc = []
+        # self.fig = plt.figure()
+
+        self.logs = []
+
+    def on_batch_end(self, batch, logs=None):
+        self.i = self.i
+
+    def on_epoch_end(self, epoch, logs={}):
+        self.logs.append(logs)
+        self.x.append(self.i)
+        self.losses.append(logs.get('loss'))
+        self.val_losses.append(logs.get('val_loss'))
+        #self.acc.append(logs.get('accuracy'))
+        self.acc.append(logs.get('prob_accuracy'))
+        #self.val_acc.append(logs.get('val_accuracy'))
+        self.val_acc.append(logs.get('val_prob_accuracy'))
+        self.i += 1
+
+    def on_train_end(self, logs):
+        clear_output(wait=True)
+        f, (gra1, gra2) = plt.subplots(1, 2, figsize=self.figsize)
+
+        # gra1.set_yscale('log')
+        gra1.plot(self.x, self.losses, label="loss")
+        gra1.plot(self.x, self.val_losses, label="val_loss")
+        gra1.title.set_text('Model Loss')
+        gra1.set_xlabel('Epoch')
+        gra1.set_ylabel('Loss')
+        # gra1.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
+        gra1.legend(['training', 'validation'], loc='upper left')
+
+        gra2.plot(self.x, self.acc, label="accuracy")
+        gra2.plot(self.x, self.val_acc, label="val_accuracy")
+        gra2.title.set_text('Model Accuracy')
+        gra2.set_xlabel('Epoch')
+        gra2.set_ylabel('Accuracy')
+        gra2.legend(['training', 'validation'], loc='upper left')
+
 
 if trainSize > 0:
     trainSet = trainSet[0:trainSize]
@@ -93,19 +146,23 @@ trainGene = trainGeneratorStones(classes_structure=classes_structure, classLevel
 valGene = trainGeneratorStones(classes_structure=classes_structure, classLevel=classLevel, batch_size=batch_size, datasetPath=datasetPath, trainSet=trainSet, aug_dict=no_augmentation_args, input_size=input_size)
 NVal = len(testSet)
 validation_steps = int(np.ceil(NVal/batch_size))
-model = jbdm_v0(input_size=input_size, num_class=len(classes))
+model = jbdm_v1.build(input_size=input_size, num_class=len(classes))
 modelFilePath = os.path.join(modelsPath, modelFileName)
 model_checkpoint = ModelCheckpoint(modelFilePath, monitor='val_loss', verbose=1, save_best_only=True)
 r.seed(1)
-history = model.fit_generator(
-    trainGene,
-    steps_per_epoch=steps_per_epoch,
-    epochs=epochs,
-    callbacks=[model_checkpoint
-        #, batch_history
-        ],
-    validation_data=valGene,
-    validation_steps=validation_steps)
+
+logdir = "logdir\\scalars\\" + datetime.now().strftime("%Y%m%d-%H%M%S")
+metrics = "grafs\\" + datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + "_v" + str(mod_ver) + "_L" + str(classLevel) + "_B"\
+          + str(batch_size) + "_E" + str(epochs) + ".png"
+
+plot = MetricsValues(figsize=(12, 6))
+
+training_history = model.fit(trainGene,
+                             steps_per_epoch=steps_per_epoch,
+                             epochs=epochs,
+                             callbacks=[model_checkpoint, batch_history, plot],
+                             validation_data=valGene,
+                             validation_steps=validation_steps)
 
 print("History:")
 print(history.history)
@@ -113,3 +170,11 @@ print(history.history)
 # print(batch_history.batch_losses)
 # print("Batch history batch_accuracies:")
 # print(batch_history.batch_accuracies)
+print("Keys:")
+print(training_history.history.keys())
+
+plt.savefig(metrics)
+plt.show()
+
+print(max(training_history.history))
+print("completed")
